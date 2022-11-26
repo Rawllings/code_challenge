ce72f1c232caedfccefa1ade170204aeae5c1a91
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-26 19:50:15 +0300

    Task submission

---
+++ README.md
@@ -0,0 +1,24 @@
+# project name:Grade Calculator
+
+
+## Project Description
+The purpose of this project is to come up with a calculator that will convert students marks into grade to determine if they have pass the exam or fail.
+
+## Project Setup
+
+### Requirement
+Visual code editor (VScode)
+HTML
+CSS
+JavaScript
+
+## How it run
+Input your score marks and get the grade
+
+## Live link
+To get to the wab page use the LINK
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,23 @@
+function calculateGrade(){
+    let grade = document.getElementById("grade").value;
+
+        if (grade >= 79) {
+           document.getElementById("result").innerHTML = `Excellent, your grade is A.`
+        }
+        else if (grade >= 60 ) {
+            document.getElementById("result").innerHTML = `Your grade is B, It's fair.`
+        }
+        else if (grade >= 49) {
+            document.getElementById("result").innerHTML = `Your grade is C, you need to put more effort.`
+        }
+        else if (grade >= 40) {
+            document.getElementById("result").innerHTML = `Your grade is D, prepare for supplementary exam.`
+        }
+        else if  (grade < 40) {
+            document.getElementById("result").innerHTML = `Your grade is E, you will not proceed to the next class.`
+        }
+        else {
+            document.getElementById("result").innerHTML = `You did not input marks`
+        }
+
+}
