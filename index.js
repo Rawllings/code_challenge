f50e9a1a3b6937caac3af8b1654817abb31dcc75
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-27 10:28:50 +0300

    Updates Commit

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function calculateGrade(){
     let grade = document.getElementById("grade").value;
 
-        if (grade >= 79) {
-           document.getElementById("result").innerHTML = `Excellent, your grade is A.`
+        if (grade > 100) {
+           document.getElementById("result").innerHTML = `Invalid Grade.`
         }
+        else if (grade >= 79) {
+            document.getElementById("result").innerHTML = `Excellent, your grade is A.`
+         }
         else if (grade >= 60 ) {
             document.getElementById("result").innerHTML = `Your grade is B, It's fair.`
         }
@@ -13,9 +16,12 @@ function calculateGrade(){
         else if (grade >= 40) {
             document.getElementById("result").innerHTML = `Your grade is D, prepare for supplementary exam.`
         }
-        else if  (grade < 40) {
+        else if  ( grade >= 0 && grade < 40 ) {
             document.getElementById("result").innerHTML = `Your grade is E, you will not proceed to the next class.`
         }
+        else if (grade < 0) {
+            document.getElementById("result").innerHTML = `Invalid Grade.`
+         }
         else {
             document.getElementById("result").innerHTML = `You did not input marks`
         }
